e0146e13ff89c9fed028b243f1d5b22535b305f3
ehsan-torabi <torabi782@gmail.com>
2023-12-30 00:58:05 +0330

    added docstrings to each function

--- Main.py
+++ Main.py
@@ -4,27 +4,29 @@ import sys
 from pathlib import Path
 from collections import defaultdict
 
-
-
 def is_music(file):
-    return file.endswith((".mp3", ".MP3"))
-
+    """Check if a file is a music file."""
+    return file.suffix.lower() == ".mp3"
 
 def get_all_music_files(paths):
+    """Get all music files in the given paths."""
+    music_files = []
     if not paths:
         paths = [os.curdir]
 
-    music_files = []
     for path in paths:
-        for child in Path(path).resolve().iterdir():
-            if child.is_dir():
-                get_all_music_files([child])
-            elif is_music(child.name):
-                music_files.append(child)
+        try:
+            for child in Path(path).resolve().iterdir():
+                if child.is_dir():
+                    music_files.extend(get_all_music_files([child]))
+                elif is_music(child):
+                    music_files.append(child)
+        except:
+            continue
     return music_files
 
-
 def get_files_state(paths):
+    """Get the state of the files in the given paths."""
     files_state = defaultdict(list)
     music_files = get_all_music_files(paths)
 
@@ -35,14 +37,9 @@ def get_files_state(paths):
 
     return files_state
 
-
 def delete_duplicates(files_state):
-    duplicate_files = []
-
-    for file_list in files_state.values():
-        if len(file_list) > 1:
-            for file in file_list[1:]:
-                duplicate_files.append(file)
+    """Delete duplicate files."""
+    duplicate_files = [file for file_list in files_state.values() if len(file_list) > 1 for file in file_list[1:]]
 
     if not duplicate_files:
         print("No duplicate files found!")
@@ -63,8 +60,8 @@ def delete_duplicates(files_state):
 
     print(f"Successfully deleted {len(duplicate_files)} duplicate files!")
 
-
 if __name__ == "__main__":
+    print("Please wait ...")
     target_dir = sys.argv[1:]
     file_states = get_files_state(target_dir)
     delete_duplicates(file_states)
